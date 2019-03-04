c29cd8aa08884cdcefc8e74f34bce45b345b38eb
Shyam Kumar <sykron@gmail.com>
2019-03-04 11:55:38 +0800

    razor pages for app + model attributes

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -9,6 +9,10 @@ namespace OdeToFood2.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        int commit();
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -25,12 +29,41 @@ namespace OdeToFood2.Data
             };
         }
 
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            restaurants.Add(newRestaurant);
+            return newRestaurant;
+        }
+
+        public int commit()
+        {
+            return 0;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
             return from r in restaurants
                    where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
         }
     }
 }
--- OdeToFood2.Core/Restaurant.cs
+++ OdeToFood2.Core/Restaurant.cs
@@ -1,10 +1,14 @@
-﻿namespace OdeToFood2.Core
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood2.Core
 {
 
     public class Restaurant
     {
         public int Id { get; set; }
+        [Required, StringLength(80)]
         public string Name { get; set; }
+        [Required, StringLength(255)]
         public string Location { get; set; }
         public CuisineType Cuisine { get; set; }
     }
